576a36f02c88695eb2eff53118afae45878e305a
Eveliina Pakarinen <pevetiainen.ep@hotmail.com>
2017-04-01 17:38:19 +0300

    Hello possu world!

--- helloWorld.py
+++ helloWorld.py
@@ -5,10 +5,9 @@ import time
 
 import scrollphat
 
-
 scrollphat.set_brightness(2)
 
-message = 'NYT ON NUKKUMAAN MENON AIKA!!!!'
+message = 'HELLO POSSU WORLD!'
 
 scrollphat.set_rotate(True)
 scrollphat.write_string(message, 11)
@@ -16,7 +15,7 @@ scrollphat.write_string(message, 11)
 while True:
     try:
         scrollphat.scroll()
-        time.sleep(0.05)
+        time.sleep(0.3)
     except KeyboardInterrupt:
         scrollphat.clear()
         sys.exit(-1)
